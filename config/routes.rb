e13edd5feac305e87bf6a07671e3b0ed4ceeb660
Hugo Dornbierer <hdornbierer@gmail.com>
2020-02-11 16:09:43 +0100

    with new record

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/show'
+  get 'articles/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles
 end
